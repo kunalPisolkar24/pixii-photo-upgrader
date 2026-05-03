d4f8de02f4983ff81ccee4d3c6ef9815aeae6c50
Kunal Pisolkar <kpisolkar24@gmail.com>
2026-05-03 07:31:17 +0000

    fix url image

--- store/use-generation-store.ts
+++ store/use-generation-store.ts
@@ -52,7 +52,7 @@ export const useGenerationStore = create<GenerationState>()(
         
         const mockImages = [
           `https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&q=80&w=800`,
-          `https://images.unsplash.com/photo-1526170315873-3a561629993d?auto=format&fit=crop&q=80&w=800`,
+          `https://images.unsplash.com/photo-1774624513295-a0bac2eb4661?q=80&w=870&auto=format`,
           `https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=800`,
           `https://images.unsplash.com/photo-1491553895911-0055eca6402d?auto=format&fit=crop&q=80&w=800`,
         ];
