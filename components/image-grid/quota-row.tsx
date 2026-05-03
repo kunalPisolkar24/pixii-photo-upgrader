32a8a14ebcf3385c2536583438da42ef68429b24
Kunal Pisolkar <kpisolkar24@gmail.com>
2026-05-03 09:28:04 +0000

    center generation counter badge on all screens

--- components/image-grid/quota-row.tsx
+++ components/image-grid/quota-row.tsx
@@ -4,7 +4,7 @@ import { GenerationCounterBadge } from "./generation-counter-badge"
 
 export function QuotaRow() {
   return (
-    <div className="mx-auto flex w-full max-w-5xl justify-center px-container sm:justify-end">
+    <div className="mx-auto flex w-full max-w-5xl justify-center px-container">
       <GenerationCounterBadge remaining={2} total={3} />
     </div>
   )
