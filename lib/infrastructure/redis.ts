aefdce6f0fc38681cc6f543cca3a93f508c02ab0
Kunal Pisolkar <kpisolkar24@gmail.com>
2026-05-04 10:38:20 +0000

    initialize upstash redis infrastructure

--- lib/infrastructure/redis.ts
+++ lib/infrastructure/redis.ts
@@ -1,10 +1,20 @@
 import { Redis } from "@upstash/redis"
 
-if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
-  throw new Error("Missing Upstash Redis configuration")
-}
+let redis: Redis | null = null
 
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+export function getRedis() {
+  if (redis) {
+    return redis
+  }
+
+  if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
+    throw new Error("Missing Upstash Redis configuration")
+  }
+
+  redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  })
+
+  return redis
+}
